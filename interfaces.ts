7da1770be2fe1fbd1282fc0e51ef040306d21930
TsnmEl <s174301@ap.be>
2026-04-07 00:09:41 +0200

    Terminal App

---
+++ interfaces.ts
@@ -0,0 +1,35 @@
+export type Difficulty = 'easy' | 'medium' | 'hard';
+
+export type RolePlaystyle = 'offensive' | 'tactical' | 'support' | 'defensive';
+
+export interface AgentRole {
+    id: string;
+    name: string;
+    playstyle: RolePlaystyle;
+    iconUrl: string;
+}
+
+export interface Agent {
+    id: string;
+    agentNumber: number;
+    name: string;
+    description: string;
+    origin: string;
+    isHuman: boolean;
+    releaseDate: string;
+    imageUrl: string;
+    fullImageUrl: string;
+    difficulty: Difficulty;
+    abilities: string[];
+    role: AgentRole;
+}
+
+export interface Role {
+    id: string;
+    name: string;
+    description: string;
+    iconUrl: string;
+    playstyle: RolePlaystyle;
+    recommendedForBeginners: boolean;
+    primaryResponsibility: string;
+}
